e106b30650bd4a00e123599dbe1f3897ffa57349
FinnVollstaedt <32217615+FinnVollstaedt@users.noreply.github.com>
2017-09-23 12:27:38 +0200

    char creator und lvlup zeugs

---
+++ game/Creator.java
@@ -0,0 +1,24 @@
+package game;
+
+public class Creator {
+
+	
+	
+	public void Heldauswählen()
+	{
+		//später
+	}
+	
+	public void Klasseauswählen()
+	{
+		//später
+	}
+	
+	public void SKPauswählen()
+	{
+		skp=10;
+		//später
+	}
+
+
+}
