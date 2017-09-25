668ce6f5dd7006d13cab759009249096cb009b84
Lennart Klein <31488915+kleinlennart@users.noreply.github.com>
2017-09-25 14:52:28 +0200

    New graphic classes with method templates

---
+++ RPG/src/graphics/Menus.java
@@ -0,0 +1,28 @@
+package graphics;
+
+import javax.swing.*;
+
+public class Menus extends JComponent {
+
+	public Menus() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public void QuestsView() {
+
+	}
+
+	public void MainMenu() {
+
+	}
+
+	public void Inventory() {
+
+	}
+
+	public void Options() {
+
+	}
+
+}
---
+++ RPG/src/graphics/Panels.java
@@ -0,0 +1,55 @@
+package graphics;
+
+import javax.swing.JComponent;
+
+public class Panels extends JComponent {
+
+	// Grafikansichten im Spiel sind Methoden
+
+	public Panels() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public void HeldCreator() {
+
+	}
+
+	public void Anmelden() {
+
+	}
+
+	public void Map() {
+
+	}
+
+	public void Stadt() {
+
+	}
+
+	public void Kampfansicht() {
+
+	}
+
+	// Buildings:
+	public void Taverne() {
+
+	}
+
+	public void Shop() {
+
+	}
+
+	public void Enchanter() {
+
+	}
+
+	public void Home() {
+
+	}
+
+	public void Schmied() {
+
+	}
+
+}
